f3b8bcd03c1562c127dd00f8ad7f36939d106d81
backstreetkiwi <mail@nikolaus-winter.de>
2015-10-14 10:47:14 +0200

    previously used locations can be selected

--- org.eclipse.m2e.e4.importview/src/org/eclipse/m2e/importview/views/ProjectImportView.java
+++ org.eclipse.m2e.e4.importview/src/org/eclipse/m2e/importview/views/ProjectImportView.java
@@ -16,13 +16,14 @@ import java.io.File;
 import java.util.List;
 
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
 import org.eclipse.m2e.core.MavenPlugin;
 import org.eclipse.m2e.core.embedder.MavenModelManager;
-import org.eclipse.m2e.core.project.AbstractProjectScanner;
 import org.eclipse.m2e.core.project.LocalProjectScanner;
 import org.eclipse.m2e.core.project.MavenProjectInfo;
+import org.eclipse.m2e.e4.importview.MavenE4ImportViewPlugin;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -45,6 +46,8 @@ public class ProjectImportView extends ViewPart {
 
    public static final String ID = "org.eclipse.m2e.importview.views.ProjectImportView";
 
+   private String rootDirectory;
+
    // UI Elements
    protected Combo rootDirectoryCombo;
    protected CheckboxTreeViewer projectTreeViewer;
@@ -62,61 +65,20 @@ public class ProjectImportView extends ViewPart {
       selectRootDirectoryLabel.setText("Root Directory");
 
       // TODO: Fill with previously used Repos
-      rootDirectoryCombo = new Combo(composite, SWT.NONE);
+      rootDirectoryCombo = new Combo(composite, SWT.READ_ONLY);
       rootDirectoryCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
       rootDirectoryCombo.setFocus();
+      rootDirectoryCombo.addSelectionListener(new RootDirectoryComboSelectionHandler());
 
       final Button browseButton = new Button(composite, SWT.NONE);
       // FIXME: externalize Strings
       // browseButton.setText(Messages.wizardImportPageBrowse);
       browseButton.setText("Browse...");
       browseButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+      browseButton.addSelectionListener(new BrowseForDirectoryHandler(parent));
 
-      // TODO: externalize Listener
-      browseButton.addSelectionListener(new SelectionAdapter() {
-         public void widgetSelected(SelectionEvent e) {
-            DirectoryDialog dialog = new DirectoryDialog(parent.getShell(), SWT.NONE);
-            // FIXME: externalize Strings, find better text
-            // dialog.setText(Messages.wizardImportPageSelectRootFolder);
-            dialog.setText("TODO");
-            String path = rootDirectoryCombo.getText();
-            if (path.length() == 0) {
-               path = ResourcesPlugin.getWorkspace().getRoot().getLocation().toPortableString();
-            }
-            dialog.setFilterPath(path);
-
-            String result = dialog.open();
-            if (result != null) {
-               rootDirectoryCombo.setText(result);
-
-               AbstractProjectScanner<MavenProjectInfo> projectScanner = getProjectScanner(result);
-
-               // FIXME: show progress to user
-               try {
-                  projectScanner.run(new NullProgressMonitor());
-               } catch (InterruptedException e1) {
-                  // FIXME: handle interruption properly
-                  System.out.println("INTERRUPTED");
-                  e1.printStackTrace();
-               }
-
-               List<MavenProjectInfo> projects = projectScanner.getProjects();
-
-               // FIXME: Sort adjacent projects alphabetically
-
-               projectTreeViewer.setInput(projects);
-               projectTreeViewer.expandAll();
-
-               // TODO: use filters
-               // projectTreeViewer.setFilters(filters);
-
-               // TODO: couple scanner to combo box
-               // if(rootDirectoryChanged()) {
-               // scanProjects();
-               // }
-            }
-         }
-      });
+      // TODO: use filters
+      // projectTreeViewer.setFilters(filters);
 
       final Label projectsLabel = new Label(composite, SWT.NONE);
       projectsLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
@@ -135,20 +97,111 @@ public class ProjectImportView extends ViewPart {
       projectTree.setLayoutData(projectTreeData);
    }
 
-   // FIXME: comments
-   // TODO: check if return type can be specialized in this case...
-   protected AbstractProjectScanner<MavenProjectInfo> getProjectScanner(String location) {
-      File root = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
-      MavenModelManager modelManager = MavenPlugin.getMavenModelManager();
-      if (location == null || location.length() == 0) {
-         return null;
-      }
-      return new LocalProjectScanner(root, location.trim(), false, modelManager);
-   }
-
    @Override
    public void setFocus() {
       // TODO: rethink which control is focused best
       rootDirectoryCombo.setFocus();
    }
+
+   /**
+    * Reads the project(s) from the given root location and fills the project selection tree viewer.
+    * 
+    * @param location root location of Maven Project Folder
+    * 
+    * @return Has a non-empty project list been loaded?
+    */
+   private boolean loadProjectSelectionList(String location) {
+
+      if (location == null || location.length() == 0) {
+         return false;
+      }
+
+      File workspaceRoot = ResourcesPlugin.getWorkspace().getRoot().getLocation().toFile();
+      MavenModelManager modelManager = MavenPlugin.getMavenModelManager();
+      LocalProjectScanner scanner = new LocalProjectScanner(workspaceRoot, location.trim(), false, modelManager);
+
+      // TODO: show progress to user (no null progress monitor, instead go async)
+      try {
+         scanner.run(new NullProgressMonitor());
+      } catch (InterruptedException e) {
+         MavenE4ImportViewPlugin.getDefault().log(IStatus.ERROR, "Scanning of projects interrupted.", e);
+         return false;
+      }
+
+      // FIXME: Sort adjacent projects alphabetically
+
+      List<MavenProjectInfo> projectList = scanner.getProjects();
+
+      if (projectList == null || projectList.isEmpty()) {
+         return false;
+      }
+
+      projectTreeViewer.setInput(projectList);
+      projectTreeViewer.expandAll();
+      return true;
+   }
+
+   /**
+    * Handles Event "Browse for Directory"
+    * 
+    * @author Nikolaus Winter, comdirect bank AG
+    */
+   private final class BrowseForDirectoryHandler extends SelectionAdapter {
+      private final Composite parent;
+
+      private BrowseForDirectoryHandler(Composite parent) {
+         this.parent = parent;
+      }
+
+      public void widgetSelected(SelectionEvent e) {
+         DirectoryDialog dialog = new DirectoryDialog(parent.getShell(), SWT.NONE);
+         // FIXME: externalize Strings
+         // dialog.setText(Messages.wizardImportPageSelectRootFolder);
+         dialog.setText("Select Root Directory");
+         dialog.setMessage("Please select root directory of Maven project(s).");
+         String currentRootDirectory = rootDirectoryCombo.getText();
+         if (currentRootDirectory.length() == 0) {
+            currentRootDirectory = ResourcesPlugin.getWorkspace().getRoot().getLocation().toPortableString();
+         }
+         dialog.setFilterPath(currentRootDirectory);
+         String newRootDirectory = dialog.open();
+         if (newRootDirectory != null) {
+            boolean nonEmptyListLoaded = loadProjectSelectionList(newRootDirectory);
+            if (!nonEmptyListLoaded) {
+               // TODO: inform user (msg)
+               return;
+            }
+            if (rootDirectoryCombo.indexOf(newRootDirectory) != -1) {
+               rootDirectoryCombo.remove(newRootDirectory);
+            }
+            rootDirectoryCombo.add(newRootDirectory, 0);
+            rootDirectoryCombo.setText(newRootDirectory);
+            rootDirectory = newRootDirectory;
+         }
+      }
+   }
+
+   /**
+    * Handles Event "Item from Root Directory Combo is selected"
+    * 
+    * @author Nikolaus Winter, comdirect bank AG
+    */
+   private final class RootDirectoryComboSelectionHandler extends SelectionAdapter {
+
+      @Override
+      public void widgetSelected(SelectionEvent e) {
+         String selectedRootDirectory = rootDirectoryCombo.getText();
+         if (selectedRootDirectory != null && selectedRootDirectory.equals(rootDirectory)) {
+            return;
+         }
+         boolean nonEmptyListLoaded = loadProjectSelectionList(selectedRootDirectory);
+         if (!nonEmptyListLoaded) {
+            // TODO: inform user (msg)
+            return;
+         }
+         rootDirectory = selectedRootDirectory;
+      }
+
+   }
+
 }
